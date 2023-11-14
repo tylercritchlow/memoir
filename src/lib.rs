d30e578776722b62dd7c13d0877bcb0e763143ce
Tyler Critchlow <tylerdarincritchlow@gmail.com>
2023-11-14 13:07:20 -0700

    start implementing a startlog fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::fs;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod startlog {
+    use std::fs::File;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn startlog(filename: &str) {
+        // Validate the filename
+        if !filename.is_empty() && filename.chars().all(|c| c.is_ascii()) {
+            // Create the file
+            match File::create(filename) {
+                Ok(_) => println!("Log file '{}' created successfully.", filename),
+                Err(e) => println!("Error creating log file: {}", e),
+            }
+        } else {
+            println!("Invalid filename provided. Please use a valid filename.");
+        }
     }
 }
+
+    #[cfg(test)]
+    mod tests {
+        use std::fs;
+        use crate::startlog::startlog;
+
+        fn test_startlog_valid_filename() {
+            let filename = "my_log.txt";
+            startlog(filename);
+
+            assert!(fs::metadata(filename).is_ok());
+        }
+
+        fn test_startlog_empty_filename() {
+            let filename = "";
+            startlog(filename);
+
+            assert!(!fs::metadata(filename).is_ok());
+        }
+
+        fn test_startlog_invalid_filename() {
+            let filename = "!@#$%^&*";
+            startlog(filename);
+
+            assert!(!fs::metadata(filename).is_ok());
+        }
+
+        fn main() {
+            test_startlog_valid_filename();
+            test_startlog_empty_filename();
+            test_startlog_invalid_filename();
+        }
+
+    }
